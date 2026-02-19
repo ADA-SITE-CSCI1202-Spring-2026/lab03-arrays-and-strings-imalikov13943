3bd97b7ad70f1ce9aa0f6388b13d85bf43b7dfad
Ilqar Malikov <melikovi908@gmail.com>
2026-02-19 10:21:23 +0400

    Submitted ex7

---
+++ ex7.java
@@ -0,0 +1,16 @@
+public class ex7 {
+    public static String reverse(String s){
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--){
+            char c = s.charAt(i);
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        System.out.println(reverse(s1));
+    }
+}
